c283eb76ec88f14eabac9a4bd5ec0dc3796475ba
NateThurmond <nathanthurmond@yahoo.com>
2024-11-22 23:52:19 -0600

    Add get mech weapons script, test extract cmd

---
+++ MechDesign/php/getMechWeapons.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../config/config.php";
+include "DB_Conn.php";
+
+$res = mysqli_query(
+    $conn,
+    "
+    SELECT weaponName, damage, heat, rangeMin, rangeShort, rangeMed, rangeLong,
+        tons, slotsRequired, ammoNeeded, weaponType, availableDate
+    FROM mechweapons WHERE 1
+    ;
+    "
+);
+
+$dataByWeaponName = [];
+$weaponArray = [];
+while ($row = mysqli_fetch_assoc($res)) {
+    $weaponArray[] = $row;
+    extract($row);
+    $data[$weaponName] = $row;
+}
+
+echo json_encode([$data, $weaponArray]);
+
+?>
