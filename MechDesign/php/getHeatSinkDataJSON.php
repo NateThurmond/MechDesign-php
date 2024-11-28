1570bb7d6d0dc1acd954445905e53c6ad52d2b3d
NateThurmond <nathanthurmond@yahoo.com>
2024-11-27 20:18:29 -0600

    Update php script with notes but remove old code

--- MechDesign/php/getHeatSinkDataJSON.php
+++ MechDesign/php/getHeatSinkDataJSON.php
@@ -1,63 +1,32 @@
 <?php
 
-    require_once("../config/config.php");
-    include("../php/DB_Conn.php");
+/*
+    Author: Nathan Thurmond
+    Revision Date: 2024-11-27
+    Notes: I'm keeping this script in case I need to reference the calcuations later but
+        the code itself is deprecated in favor of whole model approach loaded to front-end
+*/
 
-    $mechID = filter_input(INPUT_GET, 'mechIDPassed', FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? 1;
-    
-    $queryHeatSinkData = "SELECT * FROM mechinternals WHERE mechID = $mechID";
-    $heatSinkType = mysqli_query($conn, $queryHeatSinkData)->fetch_object()->heatSinkType;
-    $currentHeatWeight = mysqli_query($conn, $queryHeatSinkData)->fetch_object()->heatSinksTonnage;
-    
-    if ($heatSinkType == "Singles") {
-        $newHeatSinkType = "Doubles";
-    }
-    else { $newHeatSinkType = "Singles"; }
-    
-    if ( isset($_GET['changeHeatSink']) ) {
-        $change = $_GET['changeHeatSink'];
-        
-        if ($change == true) {
-        
-            $updateHeatSink = mysqli_query($conn, "UPDATE mechinternals SET heatSinkType = '$newHeatSinkType' WHERE mechID = $mechID");
-                if(! $updateHeatSink ) { die('Could not update data: ' . mysqli_error($conn)); }
-        }
-        unset($change);
-    }
-    
-    if ( isset($_GET['newHeatSinkNums']) ) {
-        $changeHeatSinkNum = $_GET['newHeatSinkNums'];
-        $newSinkWeight = $changeHeatSinkNum-10;
-        $newInternalsWeight = $newSinkWeight - $currentHeatWeight;
-        
-        $updateHeatSinkNum = mysqli_query($conn, "UPDATE mechinternals SET totalInternalTonnage = (totalInternalTonnage + $newInternalsWeight), heatSinksNum = $changeHeatSinkNum, heatSinksCriticals = $newSinkWeight-1, heatSinksTonnage = $newSinkWeight WHERE mechID = $mechID");
-            if(! $updateHeatSinkNum ) { die('Could not update data: ' . mysqli_error($conn)); }
-        
-        unset($changeHeatSinkNum);
-    }
-    
-    $heatSinkData = mysqli_query($conn, $queryHeatSinkData);
-    
-    $heatSinkDataArray = array();
-    if ($heatSinkData) {
-        $heatSinkDataArray = mysqli_fetch_array($heatSinkData);
-    }
-    else {   
-        die('Could not fetch data: ' . mysqli_error($conn));
-    }
-    
-    if ($heatSinkDataArray['heatSinkType'] == "Singles") {
-        $heatDissipation = $heatSinkDataArray['heatSinksNum'];
-    }
-    else { $heatDissipation = ($heatSinkDataArray['heatSinksNum'] * 2); }
+/*
+    heatSinkType = MechDesign.mechinternals.heatSinkType
+    currentHeatWeight = MechDesign.mechinternals.heatSinksTonnage
+    heatSinksNum = GET heatSinksNum
+
+    UPDATE mechinternals SET
+        totalInternalTonnage = (totalInternalTonnage + heatSinkNum - 10),
+        heatSinksNum = heatSinkNum,
+        heatSinksCriticals = heatSinkNum-1,
+        heatSinksTonnage = heatSinkNum - 10
+
+    heatDissipation = heatSinkType === Singles ? heatSinksNum : heatSinksNum * 2
     
     $heatSinkDataArrayJSON = array(
-      "heatSinkType" => $heatSinkDataArray['heatSinkType'],
-      "heatSinksNum" => $heatSinkDataArray['heatSinksNum'],
-      "heatSinksTonnage" => $heatSinkDataArray['heatSinksTonnage'],
-      "heatSinksCriticals" => $heatSinkDataArray['heatSinksCriticals'],
-      "heatDissipation" => $heatDissipation
+        "heatSinkType" => $heatSinkDataArray['heatSinkType'],
+        "heatSinksNum" => $heatSinkDataArray['heatSinksNum'],
+        "heatSinksTonnage" => $heatSinkDataArray['heatSinksTonnage'],
+        "heatSinksCriticals" => $heatSinkDataArray['heatSinksCriticals'],
+        "heatDissipation" => $heatDissipation
     );
     
     echo json_encode($heatSinkDataArrayJSON);
-    
+*/
